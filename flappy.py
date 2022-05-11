b8379fc12c1061bb4dca15fbe0e4e26ab8a9e5ab
Drystan-Furor <80721880+Drystan-Furor@users.noreply.github.com>
2022-05-11 16:18:30 +0200

    build exe
    pyinstall flappy.py

--- flappy.py
+++ flappy.py
@@ -8,6 +8,9 @@ import random  # For generating random numbers
 import sys  # We will use sys.exit to exit the program
 import pygame
 from pygame.locals import *  # Basic pygame imports
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Global Variables for the game
